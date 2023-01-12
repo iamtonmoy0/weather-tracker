ff3317df2c7b2fc3b52138e537c450f9cca0ca49
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-12 21:55:54 +0600

    hello and query func added

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 import(
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 type apiConfigData struct {
@@ -20,11 +23,23 @@ func loadApiConfig(filename string)(apiConfigData,error){
 		return apiConfigData{},err
 	}
 	var c apiConfigData
-	err=json.Unmarshal(bytes,&c)
+	err = json.Unmarshal(bytes,&c)
+
 	if err !=nil{
 		return apiConfigData{},err
 	}
 	return c , nil
+}
+func hello(w http.ResponseWriter, r *http.Request){
+	w.write([]byte("hello from go !\n"))
+}
+
+func query(city string)(weatherData,error){
+	apiConfig,err:= loadApiConfig(".apiConfig")
+	if err!= nil{
+		return weatherData{},err
+	}
+	http.Get("http://ap.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey)
 }
 func main() {
 	http.HandleFunc("/hello",hello)
@@ -36,8 +51,9 @@ func(w http.ResponseWriter, r *http.Request){
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-}
-)
+	w.Header().Set("Content-Type","application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(data)
+})
 	http.ListenAndServe(":8080",nil)
 
 }
