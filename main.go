7f10ad6174270c609cfddda0943ec2da9d375566
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-12 22:15:43 +0600

    error fixed and build completed

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
-import(
+
+import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -10,50 +11,61 @@ type apiConfigData struct {
 	OpenWeatherMapApiKey string `json:OpenWeatherMapApiKey`
 }
 
-type weatherData struct{
-	Name struct `json:"name"`
-	Main struct{
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
 		Kelvin float64 `json:"temp"`
-	}`json:"main"`
+	} `json:"main"`
 }
 
-func loadApiConfig(filename string)(apiConfigData,error){
-	bytes,err:ioutil.ReadFile(filename)
-	if err != nil{
-		return apiConfigData{},err
+func loadApiConfig(filename string) (apiConfigData, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return apiConfigData{}, err
 	}
 	var c apiConfigData
-	err = json.Unmarshal(bytes,&c)
 
-	if err !=nil{
-		return apiConfigData{},err
+	err = json.Unmarshal(bytes, &c)
+
+	if err != nil {
+		return apiConfigData{}, err
 	}
-	return c , nil
+	return c, nil
 }
-func hello(w http.ResponseWriter, r *http.Request){
-	w.write([]byte("hello from go !\n"))
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from go !\n"))
 }
 
-func query(city string)(weatherData,error){
-	apiConfig,err:= loadApiConfig(".apiConfig")
-	if err!= nil{
-		return weatherData{},err
+func query(city string) (weatherData, error) {
+	apiConfig, err := loadApiConfig(".apiConfig")
+	if err != nil {
+		return weatherData{}, err
 	}
-	http.Get("http://ap.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey)
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey + "&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+	defer resp.Body.Close()
+
+	var d weatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+	return d, nil
 }
 func main() {
-	http.HandleFunc("/hello",hello)
+	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/weather/",
-func(w http.ResponseWriter, r *http.Request){
-	city := strings.SplitN(e.Url.Path,"/",3)[2]
-	date.err := query(city)
-	if err!= nil{
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type","application/json; charset=utf-8")
-	json.NewEncoder(w).Encode(data)
-})
-	http.ListenAndServe(":8080",nil)
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := query(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(data)
+		})
+	http.ListenAndServe(":8080", nil)
 
 }
